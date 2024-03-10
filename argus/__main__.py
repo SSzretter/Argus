2d0fc5aade325e3f35438a6b5a0043a131ce8dc1
SSzretter <SSzretter@users.noreply.github.com>
2024-03-10 15:46:38 -0400

    initial fork

--- argus/__main__.py
+++ argus/__main__.py
@@ -1,6 +1,7 @@
 import asyncio
 import sys
-
+import logging
+import traceback
 import argus
 from argus.app import bot, db, engine, logger
 from argus.config import config
@@ -20,9 +21,12 @@ bot.engine = engine
 
 def main():
     try:
+       # asyncio.run(bot.run(config["bot"]["token"], log_handler=None))
         bot.run(config["bot"]["token"], log_handler=None)
     except (KeyboardInterrupt, SystemExit, RuntimeError):
         logger.info(f"Shutting Down Argus")
+    except Exception as e:
+        logger.error(traceback.format_exc())
     finally:
         sys.exit()
 
